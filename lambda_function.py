a8cb545caf86a911bb3dfbbdd111b2ac18b267fa
Bob marinko <robertpaulmarinko@yahoo.com>
2021-03-30 20:35:27 -0400

    Fixed JSON formatting of return data

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,8 @@ s3 = boto3.resource('s3')
 def respond(err, res=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
+        # 'body': err.message if err else json.dumps(res), 
+        'body': err.message if err else res, 
         'headers': {
             'Content-Type': 'application/json',
         },
